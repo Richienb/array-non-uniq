fabbf6b586ef9ec0bb0a1920c8f003dbc1303f7a
Richie Bendall <richiebendall@gmail.com>
2020-04-25 15:33:36 +1200

    Simplify implementation
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 "use strict"
 
 module.exports = (array) => {
-	if (!Array.isArray(array)) throw new TypeError("An array must be provided!")
-	const duplicates = []
-	array.forEach((value, index, array) => {
-		if ((array.indexOf(value) !== index || array.lastIndexOf(value) !== index) && !duplicates.includes(value)) duplicates.push(value)
-	})
-	return duplicates
+	if (!Array.isArray(array)) {
+		throw new TypeError("An array must be provided!")
+	}
+
+	return array.filter((value, index) => array.indexOf(value) === index && array.lastIndexOf(value) !== index)
 }
